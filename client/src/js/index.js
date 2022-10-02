83740b1e55e7033d19e72a475b47c15ea941173b
Ed <105404937+indy6678@users.noreply.github.com>
2022-10-02 17:08:03 -0500

    added webpack

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,23 @@
+// entry point
+
+// import js files
 import "./form";
 import "./submit";
+
+// import CSS files
+import "../css/index.css";
+
+// import Bootstrap
+import {Tooltip, Toast, Popover} from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+import Logo from '../images/lightning.jpg'
+
+window.addEventListener('load', function() {
+    this.document.getElementById('logo').src = Logo;
+    this.document.getElementById('bearThumbnail').src = Bear;
+    document.getElementById('dogThumbnail').src = Dog;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: "./src/js/index.js",
     output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
-    mode: "development",
-}
+    module: {
+        rules: [
+            {   // handle images
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            { // webpack loaders
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    },
+                },
+            },
+        ],
+    },
+};
